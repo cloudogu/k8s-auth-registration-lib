2e20908e3aea277dfc288c566e72cd21ebb3f992
Benjamin Ernst <benjamin.ernst@cloudogu.com>
2026-02-23 12:41:21 +0100

    #1 add basic crd manifests

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -4,7 +4,7 @@ This file was generated with "make generate-deepcopy".
 
 // Package v1 contains API Schema definitions for the auth v1 API group.
 // +kubebuilder:object:generate=true
-// +groupName=auth.cloudogu.com
+// +groupName=k8s.cloudogu.com
 package v1
 
 import (
@@ -14,7 +14,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "auth.cloudogu.com", Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: "k8s.cloudogu.com", Version: "v1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
